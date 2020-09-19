bb764925e7407024f34023a6f07515dbb931f73c
NguyenPhatThanhTai <57514749+NguyenPhatThanhTai@users.noreply.github.com>
2020-09-19 15:02:54 +0700

    wrong

--- untitled1/MinMax.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $Array = array(2,3,1,5,8,5,9,3);
-    $Min = min($Array);
-    $Max = max($Array);
-    $Sum = array_sum($Array);
-
-    echo $Min . "-" . $Max . "-" . $Sum;
--- untitled1/Start.php
+++
@@ -1,4 +0,0 @@
-<?php
-    echo $_REQUEST["First_Input"] . "<Br/>";
-    echo $_REQUEST["First_Password"];
-
